a4dbeaecb2531a4904fd951a7b8b0c5b631b79ad
Waldener Monteiro <jjrr019@gmail.com>
2020-10-10 00:52:58 -0300

    feature(crimes): apresentando o filtro e a listagem dos crimes

---
+++ src/pages/crimes/List.vue
@@ -0,0 +1,31 @@
+<template>
+  <v-container>
+    <v-row class="">
+      <v-col class="d-flex justify-start">
+        <div class="text-uppercase font-30">Crime List</div>
+      </v-col>
+      <v-col class="d-flex justify-end">
+        <v-btn class="default-color">
+          <v-icon left dark>
+            mdi-plus
+          </v-icon>
+          add a new crime
+        </v-btn>
+      </v-col>
+    </v-row>
+    <CFilter> </CFilter>
+  </v-container>
+</template>
+<script>
+import CFilter from './components/Filter'
+export default {
+  components: {
+    CFilter
+  },
+  data () {
+    return {
+      firstName: ''
+    }
+  }
+}
+</script>
