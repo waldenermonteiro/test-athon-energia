96cff72441efd3c1207f96291b423149af07eb75
Waldener Monteiro <jjrr019@gmail.com>
2020-10-10 00:53:11 -0300

    feature: criando componente de layout

---
+++ src/layouts/Dashboard.vue
@@ -0,0 +1,32 @@
+<template>
+  <v-app>
+    <v-app-bar dense app color="#FCFC99" dark>
+      <div class="d-flex align-center">
+        <v-img
+          alt="Vuetify Logo"
+          class="shrink mr-2"
+          contain
+          src="../assets/logo.png"
+          transition="scale-transition"
+          width="80"
+        />
+      </div>
+
+      <v-spacer></v-spacer>
+    </v-app-bar>
+
+    <v-main>
+      <router-view />
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: 'App',
+
+  data: () => ({
+    //
+  })
+}
+</script>
