4aa92bec54cc4486fca66d76604093fa710bcc0f
Waldener Monteiro <jjrr019@gmail.com>
2020-10-11 00:08:36 -0300

    refactor: adicionando os stores no index

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,15 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 
+import Crime from '../pages/crimes/store'
+import Criminal from '../pages/criminals/store'
+import Weapon from '../pages/weapons/store'
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
   modules: {
+    Crime,
+    Criminal,
+    Weapon
   }
 })
