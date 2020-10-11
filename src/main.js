2387f5dc44c059a5bc7935481575494bcdfdd06a
Waldener Monteiro <jjrr019@gmail.com>
2020-10-11 00:07:31 -0300

    refactor: instalando vários módulos para auxiliar no projeto

--- src/main.js
+++ src/main.js
@@ -3,9 +3,15 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import vuetify from './plugins/vuetify'
+import BaseVuex from './plugins/baseVuex'
+import Vuelidate from 'vuelidate'
+import VueLoading from 'vuejs-loading-plugin'
 import './assets/index.scss'
 Vue.config.productionTip = false
 
+Vue.use(BaseVuex)
+Vue.use(Vuelidate)
+Vue.use(VueLoading, { text: 'Loading' })
 new Vue({
   router,
   store,
