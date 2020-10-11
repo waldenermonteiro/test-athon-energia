0cb62a61d649a71baa93138605f885646470685f
Waldener Monteiro <jjrr019@gmail.com>
2020-10-11 00:09:13 -0300

    refactor(crimes): chamando o componente de cadastro

--- src/pages/crimes/List.vue
+++ src/pages/crimes/List.vue
@@ -5,7 +5,7 @@
         <div class="text-uppercase font-30">Crime List</div>
       </v-col>
       <v-col class="d-flex justify-end">
-        <v-btn class="default-color">
+        <v-btn @click="openModalCreate()" class="default-color cinzel-font">
           <v-icon left dark>
             mdi-plus
           </v-icon>
@@ -14,17 +14,25 @@
       </v-col>
     </v-row>
     <CFilter> </CFilter>
+    <CCreate ref="modalCreate"> </CCreate>
   </v-container>
 </template>
 <script>
+import CCreate from './Create'
 import CFilter from './components/Filter'
 export default {
   components: {
+    CCreate,
     CFilter
   },
   data () {
     return {
       firstName: ''
+    }
+  },
+  methods: {
+    openModalCreate () {
+      this.$refs.modalCreate.openModal()
     }
   }
 }
