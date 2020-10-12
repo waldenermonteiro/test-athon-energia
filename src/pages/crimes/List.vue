68d08ac73de4a2e4035b4d2e9706fe6a20deaa34
Waldener Monteiro <jjrr019@gmail.com>
2020-10-12 00:04:38 -0300

    refactor(crimes): refatorando a estrutura e adicionando o componente card-item

--- src/pages/crimes/List.vue
+++ src/pages/crimes/List.vue
@@ -5,7 +5,7 @@
         <div class="text-uppercase font-30">Crime List</div>
       </v-col>
       <v-col class="d-flex justify-end">
-        <v-btn @click="openModalCreate()" class="default-color cinzel-font">
+        <v-btn @click="openModalCreate()" class="default-color">
           <v-icon left dark>
             mdi-plus
           </v-icon>
@@ -13,17 +13,36 @@
         </v-btn>
       </v-col>
     </v-row>
-    <CFilter> </CFilter>
+    <CFilter></CFilter>
+    <v-row>
+      <v-col cols="12" sm="4" md="4" lg="3" v-for="n in 8" :key="n">
+        <ListItem>
+          <div class="mb-5">
+            <v-icon class="float-right" color="#2699fb" @click="openModalListItemDetails()">
+              mdi-magnify-plus-outline
+            </v-icon>
+            <v-icon class="test float-right" color="#2699fb" @click="clickDeleteIcon()">
+              mdi-delete
+            </v-icon>
+          </div>
+        </ListItem>
+      </v-col>
+    </v-row>
     <CCreate ref="modalCreate"> </CCreate>
+    <ListItemDetails ref="modalListItemDetails"></ListItemDetails>
   </v-container>
 </template>
 <script>
 import CCreate from './Create'
 import CFilter from './components/Filter'
+import ListItem from './components/ListItem'
+import ListItemDetails from './components/ListItemDetails'
 export default {
   components: {
     CCreate,
-    CFilter
+    CFilter,
+    ListItem,
+    ListItemDetails
   },
   data () {
     return {
@@ -33,6 +52,9 @@ export default {
   methods: {
     openModalCreate () {
       this.$refs.modalCreate.openModal()
+    },
+    openModalListItemDetails () {
+      this.$refs.modalListItemDetails.openModal()
     }
   }
 }
