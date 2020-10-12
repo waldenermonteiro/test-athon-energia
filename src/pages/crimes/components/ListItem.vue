292d2525a7f3d5c150d8f1d6c4ea39f1781c32cf
Waldener Monteiro <jjrr019@gmail.com>
2020-10-12 00:03:34 -0300

    feature(crimes): criando componente de card-item

---
+++ src/pages/crimes/components/ListItem.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-card class="main-color">
+    <v-card-title class="item-details-title pb-3"> Ideological Falsehood </v-card-title>
+    <v-card-subtitle class="pb-2">Crime Type</v-card-subtitle>
+    <v-card-text>
+      <p class="mb-0 font-weight-bold">2020/07/03 - 00:00:00</p>
+      <p class="mb-1">Paraguai</p>
+      <slot> </slot>
+    </v-card-text>
+  </v-card>
+</template>
+<script>
+export default {
+  methods: {
+  }
+}
+</script>
