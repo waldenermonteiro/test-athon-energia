71caeb912f4d393f1e758b7dddac5e8ba16a5862
Waldener Monteiro <jjrr019@gmail.com>
2020-10-13 21:48:41 -0300

    refactor(crimes): retirando classes desnecessárias e colocando as corretas

--- src/pages/crimes/components/ListItem.vue
+++ src/pages/crimes/components/ListItem.vue
@@ -1,17 +1,16 @@
 <template>
   <v-card class="main-color">
-    <v-card-title class="item-details-title pb-3"> Ideological Falsehood </v-card-title>
-    <v-card-subtitle class="pb-2">Crime Type</v-card-subtitle>
+    <v-card-title class="item-details-title pb-4"> Ideological Falsehood </v-card-title>
+    <v-card-subtitle class="item-details-subtitle pb-2">Crime Type</v-card-subtitle>
     <v-card-text>
-      <p class="mb-0 font-weight-bold">2020/07/03 - 00:00:00</p>
-      <p class="mb-1">Paraguai</p>
+      <p class="item-details-text mb-0">2020/07/03 - 00:00:00</p>
+      <p class="item-details-subtitle mb-1">Paraguai</p>
       <slot> </slot>
     </v-card-text>
   </v-card>
 </template>
 <script>
 export default {
-  methods: {
-  }
+  methods: {}
 }
 </script>
