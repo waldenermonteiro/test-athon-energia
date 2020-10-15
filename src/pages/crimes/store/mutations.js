0bb562972cc4c5d5dcec3ff4e2b26a5f0dd37012
Waldener Monteiro <jjrr019@gmail.com>
2020-10-14 21:48:02 -0300

    refactor(crimes): adicionando método para deixar iniciais maiúsculas, e mutation para setar a listagem de crimes

--- src/pages/crimes/store/mutations.js
+++ src/pages/crimes/store/mutations.js
@@ -1,3 +1,19 @@
+function firstUppercase (text) {
+  var words = text.toLowerCase().split(' ')
+  for (var a = 0; a < words.length; a++) {
+    var w = words[a]
+    words[a] = w[0].toUpperCase() + w.slice(1)
+    return words.join(' ')
+  }
+}
+export const SET_DATA = (state, payload) => {
+  state.crimes = payload.crimes
+}
+
 export const SET_DATA_CRIME_TYPES = (state, payload) => {
-  state.crimeTypes = payload.crime_types
+  payload.crime_types.map((type) => {
+    type.tx_type = firstUppercase(type.tx_type)
+    return type
+  })
+  state.typesOfCrime = payload.crime_types
 }
