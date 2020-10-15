ade338d33d6ebd5c0b791258d98d3ebdd426e688
Waldener Monteiro <jjrr019@gmail.com>
2020-10-14 21:47:02 -0300

    refactor: retirando chamada do heroku desnecessária

--- src/plugins/axios.js
+++ src/plugins/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const HTTPClient = axios.create({
-  baseURL: 'https://cors-anywhere.herokuapp.com/https://athon-tech-ps.herokuapp.com/',
+  baseURL: 'https://athon-tech-ps.herokuapp.com/',
   headers: {
     'Content-Type': 'application/json',
     'Access-Control-Allow-Origin': '*',
