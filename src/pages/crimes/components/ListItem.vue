2f0b7b0b5c16052a09656925c38c9da11c951cd2
Waldener Monteiro <jjrr019@gmail.com>
2020-10-14 21:48:47 -0300

    refactor(crimes): adicionando props para o componente

--- src/pages/crimes/components/ListItem.vue
+++ src/pages/crimes/components/ListItem.vue
@@ -1,16 +1,28 @@
 <template>
   <v-card class="main-color">
-    <v-card-title class="item-details-title pb-4"> Ideological Falsehood </v-card-title>
+    <v-card-title class="item-details-title pb-4"> {{typeOfCrime}} </v-card-title>
     <v-card-subtitle class="item-details-subtitle pb-2">Crime Type</v-card-subtitle>
     <v-card-text>
-      <p class="item-details-text mb-0">2020/07/03 - 00:00:00</p>
-      <p class="item-details-subtitle mb-1">Paraguai</p>
+      <p class="item-details-text mb-0">{{$formatApiDateTimeToFront(dateOfTheCrime)}}</p>
+      <p class="item-details-subtitle mb-1">{{country}}</p>
       <slot> </slot>
     </v-card-text>
   </v-card>
 </template>
 <script>
+import masks from '../../../mixins/masks'
 export default {
-  methods: {}
+  mixins: [masks],
+  props: {
+    typeOfCrime: {
+      required: true
+    },
+    dateOfTheCrime: {
+      required: true
+    },
+    country: {
+      required: true
+    }
+  }
 }
 </script>
