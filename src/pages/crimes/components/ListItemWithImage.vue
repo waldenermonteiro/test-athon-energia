50e882fa4214201a4e01fc4b10500c671f056d34
Waldener Monteiro <jjrr019@gmail.com>
2020-10-17 00:15:16 -0300

    refactor(crimes): adicionando imagens randomicas

--- src/pages/crimes/components/ListItemWithImage.vue
+++ src/pages/crimes/components/ListItemWithImage.vue
@@ -1,19 +1,19 @@
 <template>
-  <v-row>
-    <v-col cols="12" sm="6" md="6" v-for="n in 2" :key="n">
-      <v-row no-gutters>
-        <v-col cols="12" sm="3" lg="4" class="d-flex align-center">
-          <v-avatar class="width-image"> <img alt="user" src="https://cdn.pixabay.com/photo/2020/06/24/19/12/cabbage-5337431_1280.jpg" /> </v-avatar
-          ></v-col>
-        <v-col cols="12" sm="9" lg="8" class="d-flex align-center height-col">
-          <v-card class="main-color elevation-0">
-            <v-card-title v-if="!onlyOne" class="item-details-title-no-bold mb-0 pb-3"> Voldemort </v-card-title>
-            <v-card-subtitle v-if="!onlyOne" class="item-details-subtitle pb-1">Criminal</v-card-subtitle>
-            <v-card-title class="item-details-title-no-bold pt-0 pb-3"> Dark Magic Wand </v-card-title>
-            <v-card-subtitle class="item-details-subtitle">Ranged Weapon</v-card-subtitle>
-          </v-card>
-        </v-col>
-      </v-row>
+  <v-row no-gutters>
+    <v-col cols="12" sm="3" lg="4" class="d-flex align-center">
+      <v-avatar v-if="emptyItems" class="width-image white d-flex align-center "
+      ><p class="black--text font-weight-black pt-3 " v-html="emptyItems">
+      </p>
+      </v-avatar>
+      <v-avatar v-else class="width-image"> <img alt="user" :src="`https://randomuser.me/api/portraits/med/${randomSex}/${randomId}.jpg`" /> </v-avatar
+      ></v-col>
+    <v-col cols="12" sm="9" lg="8" class="d-flex align-center height-col">
+      <v-card class="main-color elevation-0">
+        <v-card-title v-if="!onlyOne" class="item-details-title-no-bold mb-0 pb-3"> {{ firstTitle }} </v-card-title>
+        <v-card-subtitle v-if="!onlyOne" class="item-details-subtitle pb-1">Criminal</v-card-subtitle>
+        <v-card-title class="item-details-title-no-bold pt-0 pb-3"> {{ secondTitle }} </v-card-title>
+        <v-card-subtitle class="item-details-subtitle">{{ subtitle }}</v-card-subtitle>
+      </v-card>
     </v-col>
   </v-row>
 </template>
@@ -23,7 +23,29 @@ export default {
     onlyOne: {
       required: false,
       default: false
+    },
+    firstTitle: {
+      required: false
+    },
+    secondTitle: {
+      required: true
+    },
+    subtitle: {
+      required: true
+    },
+    emptyItems: {
+      required: false
     }
+  },
+  data () {
+    return {
+      randomId: 0,
+      randomSex: ''
+    }
+  },
+  mounted () {
+    this.randomId = Math.floor(Math.random() * 10)
+    this.randomSex = this.randomId % 2 === 0 ? 'women' : 'men'
   }
 }
 </script>
