a7309630e8226abe7c6bd22034f2a832211fbc40
Waldener Monteiro <jjrr019@gmail.com>
2020-10-18 20:23:00 -0300

    feature(victims): criando a store e service para o módulo victim

---
+++ src/pages/victims/services/VictimService.js
@@ -0,0 +1,8 @@
+import base from '../../../services/base'
+
+class Victims extends base {
+  constructor () {
+    super('/victims')
+  }
+}
+export default new Victims()
--- src/store/index.js
+++ src/store/index.js
@@ -3,6 +3,7 @@ import Vuex from 'vuex'
 
 import Crime from '../pages/crimes/store'
 import Criminal from '../pages/criminals/store'
+import Victim from '../pages/victims/store'
 import Weapon from '../pages/weapons/store'
 Vue.use(Vuex)
 
@@ -10,6 +11,7 @@ export default new Vuex.Store({
   modules: {
     Crime,
     Criminal,
+    Victim,
     Weapon
   }
 })
